710db9b4a6c27d16185616063cf27c0f276341d1
s19424 <s19424@pjwstk.edu.pl>
2022-04-16 19:39:52 +0200

    Added test delete method for address repository

--- OneBanTMS.IntegrationTests/AddressRepository.Test.cs
+++ OneBanTMS.IntegrationTests/AddressRepository.Test.cs
@@ -125,5 +125,57 @@ namespace OneBanTMS.IntegrationTests
                     && x.AdrCountry == addressDtoToUpdate.AdrCountry);
             Assert.That(addressCount, Is.EqualTo(1));
         }
+
+        [Test, Isolated]
+        public async Task DeleteAddress_ExistingId_ShouldDeleteFromDatabase()
+        {
+            CompanyDto companyDto = new CompanyDto()
+            {
+                CmpName = "TestCompany",
+                CmpNip = "9999999999",
+                CmpKrsNumber = "",
+                CmpLandline = "",
+                CmpRegon = "",
+                CmpNipPrefix = "PL"
+            };
+            var companyRepository = new CompanyRepository(_context, _validator);
+            await companyRepository.AddNewCompany(companyDto);
+            var companyId = await _context
+                .Companies
+                .Where(x => x.CmpNip == companyDto.CmpNip)
+                .Select(x => x.CmpId)
+                .SingleOrDefaultAsync();
+            AddressDto addressDto = new AddressDto()
+            {
+                AdrTown = "Test",
+                AdrStreet = "Test",
+                AdrStreetNumber = "Test",
+                AdrPostCode = "Test",
+                AdrCountry = "Test"
+            };
+            var addressRepository = new AddressRepository(_context, companyRepository);
+            await addressRepository.AddNewAddress(addressDto, companyId);
+            var addressId = await _context
+                .Addresses
+                .Where(x =>
+                    x.AdrTown == addressDto.AdrTown
+                    && x.AdrStreet == addressDto.AdrStreet
+                    && x.AdrStreetNumber == addressDto.AdrStreetNumber
+                    && x.AdrPostCode == addressDto.AdrPostCode
+                    && x.AdrCountry == addressDto.AdrCountry)
+                .Select(x =>
+                    x.AdrId)
+                .SingleOrDefaultAsync();
+            await addressRepository.DeleteAddress(addressId);
+            var countAddresses = await _context
+                .Addresses
+                .CountAsync(x =>
+                    x.AdrTown == addressDto.AdrTown
+                    && x.AdrStreet == addressDto.AdrStreet
+                    && x.AdrStreetNumber == addressDto.AdrStreetNumber
+                    && x.AdrPostCode == addressDto.AdrPostCode
+                    && x.AdrCountry == addressDto.AdrCountry);
+            Assert.That(countAddresses, Is.EqualTo(0));
+        }
     }
 }
