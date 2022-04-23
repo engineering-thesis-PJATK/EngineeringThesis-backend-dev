e9d1e39a48e8b07b7be6c7632efde877276a1136
s19424 <s19424@pjwstk.edu.pl>
2022-04-23 11:17:21 +0200

    Added UpdateCustomer_PassValid_ShouldUpdateCustomerInDatabase method for customer repository integration tests

--- OneBanTMS.IntegrationTests/CustomerRepository.Test.cs
+++ OneBanTMS.IntegrationTests/CustomerRepository.Test.cs
@@ -127,5 +127,53 @@ namespace OneBanTMS.IntegrationTests
             await action.Should().ThrowExactlyAsync<ArgumentException>()
                 .WithMessage("Company not exists");
         }
+
+        [Test, Isolated]
+        public async Task UpdateCustomer_PassValid_ShouldUpdateCustomerInDatabase()
+        {
+            var companyRepository = new CompanyRepository(_context, new CompanyValidator(new CompanyHandler(_context)));
+            var customerRepository = new CustomerRepository(_context, _validator, companyRepository);
+            
+            CustomerDto newCustomerDto = new CustomerDto()
+            {
+                CurName = "TestName",
+                CurSurname = "TestSurname",
+                CurEmail = "Test@TestEmail.com",
+                CurComments = "TestComments",
+                CurPosition = "TestPosition",
+                CurPhoneNumber = "000000000"
+            };
+            CompanyDto newCompanyDto = new CompanyDto()
+            {
+                CmpName = "TestCompany",
+                CmpNip = "0000000000",
+                CmpNipPrefix = "PL"
+            };
+            await companyRepository.AddNewCompany(newCompanyDto);
+            var companyId = await _context
+                .Companies
+                .Where(x =>
+                    x.CmpNip == newCompanyDto.CmpNip)
+                .Select(x => x.CmpId)
+                .SingleOrDefaultAsync();
+            await customerRepository.AddNewCustomer(newCustomerDto, companyId);
+            var customerId = await _context
+                .Customers
+                .Where(x =>
+                    x.CurEmail == newCustomerDto.CurEmail)
+                .Select(x => x.CurId)
+                .SingleOrDefaultAsync();
+            
+            CustomerDto customer = new CustomerDto()
+            {
+                CurName = "TestTestName",
+                CurSurname = "TestTestSurname",
+                CurEmail = "TestTest@TestEmail.com",
+                CurComments = "TestTestComments",
+                CurPosition = "TestTestPosition",
+                CurPhoneNumber = "1111111111"
+            };
+            await customerRepository.UpdateCustomer(customer, customerId);
+        }
     }
 }
